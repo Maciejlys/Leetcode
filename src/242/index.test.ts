001db4559a0536110adabff5917e0590c23999c8
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2023-07-10 22:59:19 +0200

    242

---
+++ src/242/index.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from 'vitest';
+import testing from '.';
+
+describe('242', () => {
+  test('should pass', () => {
+    const s = 'anagram',
+      t = 'nagaram';
+    const output = true;
+
+    const result = testing(s, t);
+
+    expect(result).toStrictEqual(output);
+  });
+  test('should pass', () => {
+    const s = 'rat',
+      t = 'car';
+    const output = false;
+
+    const result = testing(s, t);
+
+    expect(result).toStrictEqual(output);
+  });
+});
